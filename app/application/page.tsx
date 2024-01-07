5a3aeb737f66974df5b596375b872bbb0ae4dc16
Armin <Armin.Fullstack@gmail.com>
2024-01-07 21:44:45 +0330

    feat: implemneted the <Sidebar /> UI

--- app/application/page.tsx
+++ app/application/page.tsx
@@ -1,3 +1,10 @@
+import Sidebar from "../ui/sidebar";
+
 export default function Page() {
-  return <div>app</div>;
+  return (
+    <div className="grid grid-cols-2">
+      <Sidebar />
+      <div className="bg-gray-500">Map</div>
+    </div>
+  );
 }
--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,7 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo() {
+export default function Logo({ type }) {
+  if (type === "centerLogo")
+    return (
+      <Link href={"/"}>
+        <Image
+          src="/logo.png"
+          width={150}
+          height={100}
+          alt="logo"
+          className="mx-auto mb-7"
+        />
+      </Link>
+    );
+
   return (
     <Link href={"/"}>
       <Image src="/logo.png" width={150} height={100} alt="logo" />
